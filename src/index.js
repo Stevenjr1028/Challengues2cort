198c8834c6a2b183df2c65f81a6178fdbb0fd8eb
Gerson Steven <98818664+Stevenjr1028@users.noreply.github.com>
2023-04-22 14:26:46 -0500

    Parte final

--- src/index.js
+++ src/index.js
@@ -1,5 +1,17 @@
-import ReactDOM from "react-dom/client";
-import App from "./App.js";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+import { store } from './store/store';
+import Registro from './components/registro';
+import { Upload } from './components/upload';
 
-let root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <Provider store={store}>
+        <BrowserRouter>
+            <Registro />
+            <Upload />
+        </BrowserRouter>
+    </Provider>
+);
